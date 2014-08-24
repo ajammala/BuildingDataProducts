8196aba8c176f5a2dcfacd586fee7919f24c23b0
Adi J <aditya.jammalamadaka@gmail.com>
2014-08-24 02:33:29 -0400

    First Submission

---
+++ server.R
@@ -0,0 +1,67 @@
+library(shiny)
+library(ggplot2)
+
+shinyServer(
+  function(input, output) {   
+    filedata <- reactive({
+        myCsv <- getURL("https://gist.github.com/raw/667867/c47ec2d72801cfd84c6320e1fe37055ffe600c87/test.csv")
+        temporaryFile <- tempfile()
+        con <- file(temporaryFile, open = "w")
+        cat(myCsv, file = con) 
+        close(con)
+        
+        read.csv(temporaryFile)
+    })
+    
+    output$state <- renderUI({
+      df <-filedata()
+      State <- levels(factor(df$State))
+      State <- c("", State)
+      selectInput("state", "Choose State: ", State)
+    })
+    
+    output$bankPlot <- renderPlot({
+      df <- filedata()
+      df$TotalDeposits <- as.numeric(gsub(",","", df$TotalDeposits))
+      df$TotalAssets <- as.numeric(gsub(",","",df$TotalAssets))
+      df <- subset(df, TotalAssets >= as.numeric(input$assetsize[1]) & TotalAssets <= as.numeric(input$assetsize[2]))
+      df$EffectiveDate <- as.Date(as.character(df$EffectiveDate), "%m/%d/%Y")
+      df$year <- format(df$EffectiveDate, "%Y")
+      if(is.null(input$state)) return (NULL)
+      if(input$state != "")
+        df <- df[df$State==input$state,]
+      if(input$trantype == "Assistance"){
+        df <- df[df$FailureAssistance=="ASSISTANCE",]
+      }
+      if(input$trantype == "Failure"){
+        df <- df[df$FailureAssistance=="FAILURE",]
+      }
+      df <- subset(df, EffectiveDate >= as.Date(input$fromdate) & EffectiveDate <= as.Date(input$todate))
+      plotdata <- aggregate(InstitutionName~year+CharterClass, data=df, FUN=length)
+      ggplot(plotdata, aes(year, weight = InstitutionName)) + geom_bar(aes(fill=CharterClass)) + ggtitle("Bank Failure/Assistance Transactions In the US per Year") + ylab("Number of Failures/Assistance Transactions") + xlab("Year")
+    })
+    
+    output$filetable <- renderDataTable({
+      df <- filedata()
+      df$TotalDeposits <- as.numeric(gsub(",","", df$TotalDeposits))
+      df$TotalAssets <- as.numeric(gsub(",","",df$TotalAssets))
+      df <- subset(df, TotalAssets >= as.numeric(input$assetsize[1]) & TotalAssets <= as.numeric(input$assetsize[2]))
+      df$EffectiveDate <- as.Date(as.character(df$EffectiveDate), "%m/%d/%Y")
+      df$year <- format(df$EffectiveDate, "%Y")
+      if(is.null(input$state)) return (NULL)
+      if(input$state != "")
+        df <- df[df$State==input$state,]
+      if(input$trantype == "Assistance"){
+        df <- df[df$FailureAssistance=="ASSISTANCE",]
+      }
+      if(input$trantype == "Failure"){
+        df <- df[df$FailureAssistance=="FAILURE",]
+      }
+      df <- subset(df, EffectiveDate >= as.Date(input$fromdate) & EffectiveDate <= as.Date(input$todate))
+      df <- df[with(df, order(-TotalAssets)), ] 
+      df <- df[, -9]
+      colnames(df) <- c("Institution Name","City", "State", "Effective Date", "Charter Class", "Failure/Assistance", "Total Deposits (* $1000)", "Total Assets (* $1000)")
+      df
+    }, options = list(bFilter = FALSE))
+  }
+)
---
+++ ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+
+shinyUI(pageWithSidebar(
+  headerPanel("Failures & Assistance Transactions In the US From 1970 - 2014"),
+  sidebarPanel(
+    h5('Selection: '),
+    dateInput("fromdate", "From Date:", min = "1970-01-01", max = "2015-01-01", value = "1970-01-01", format = "mm/dd/yyyy"),
+    dateInput("todate", "To Date:", min = "1970-01-01", max = "2015-01-01", value = "2014-08-01", format = "mm/dd/yyyy"),
+    uiOutput("state"),
+    radioButtons("trantype", "Transaction Type:", choices=c("Both","Assistance","Failure"), selected = "Both"),
+    sliderInput("assetsize", "Total Asset Size (*1000):", min = 0, max = 2000000000, value = c(1000000, 2000000000)),
+    submitButton('Submit')
+  ),
+  mainPanel(
+    plotOutput("bankPlot"),
+    h4('Bank Failures & Assistance Transactions (sorted by Asset Size)'),
+    dataTableOutput("filetable")
+  )
+))
